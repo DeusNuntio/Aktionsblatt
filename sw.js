3465ac49d0049a0acaedc64737fdc47c0eb3f7ef
DeusNuntio <deusnuntio@googlemail.com>
2026-09-07 23:38:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+/* Aktionsblatt – Service Worker, Version 0.5 */
+const CACHE = "aktionsblatt-v0.5";
+const DATEIEN = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", ev => {
+  ev.waitUntil(caches.open(CACHE).then(c => c.addAll(DATEIEN)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", ev => {
+  ev.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", ev => {
+  if (ev.request.method !== "GET") return;
+  ev.respondWith(
+    fetch(ev.request)
+      .then(antwort => {
+        const kopie = antwort.clone();
+        caches.open(CACHE).then(c => c.put(ev.request, kopie)).catch(() => {});
+        return antwort;
+      })
+      .catch(() => caches.match(ev.request).then(t => t || caches.match("./index.html")))
+  );
+});
